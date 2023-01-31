7f9bd502bb36f5862b552b4a522eb6facc468a49
wjchulme <25637345+wjchulme@users.noreply.github.com>
2023-01-31 12:14:42 +0000

    walking skeleton

---
+++ analysis/standardisation.R
@@ -0,0 +1,341 @@
+######################################
+# Direct age-sex (or any other strata) standardisation of event rates
+# Standardisation works as follows:
+# Strata-specific rates are calculated in a given cohort of interest (eg rates as provided by measures framework)
+# This cohort may not reflect the age-sex (or whatever) distribution of the target population of interest, or is not the same as another cohort to be compared
+# To deal with this, we can standardise overall rates to a reference population,
+# by assuming that the strata-specific rates are the those that would be observed in equivalent strata in the reference population
+# Essentially, the overall rates are calculated as a weighted-mean of the strata-specific rates, with weights defined by the relative size of the strata in the reference population
+#
+# The script proceeds as follows:
+# Import the arguments
+# Import cohort rates data, stratified by pre-defined strata
+# Import reference population data, stratified by the same pre-defined strata
+# Calculate the weighted average of the rates (= standardise the rates), overall and in all subgroups of interest
+# Output unstandardised and standardised rates + plots if requested
+
+######################################
+
+# preliminaries ----
+## import libraries ----
+library('tidyverse')
+library('lubridate')
+library('here')
+library('glue')
+library("optparse")
+
+
+## arguments -----
+
+
+
+
+
+## parse command-line arguments ----
+
+args <- commandArgs(trailingOnly=TRUE)
+
+if(length(args)==0){
+  # use for interactive testing
+  df_input <- "output/measures/measure_death.csv"
+  dir_output <-  "output/standardised/"
+  standardisation_strata <- "year;sex;ageband5year;region"
+  subgroups <- "overall;sex;ageband20year;region"
+  numerator <- "death_1year"
+  denominator <- "population"
+  reference_population <- "ONS-England-mid-year-pop"
+  min_count <- as.integer("6")
+  round_method <- "mid"
+  make_plots <- TRUE
+
+} else {
+
+  option_list <- list(
+    make_option("--df_input", type = "character", default = NULL,
+                help = "Input dataset .csv filename [default %default]. (ideally should use feather to preserve factor levels -- to discuss)",
+                metavar = "filename.feather"),
+    make_option("--dir_output", type = "character", default = NULL,
+                help = "Output directory [default %default].",
+                metavar = "output"),
+    make_option("--standardisation_strata", type = "character", default = NULL,
+                help = "Strata used to standardise rates, separated by semi-colon (;) [default %default]. Usually age-sex-year, but could include region, ethnicity, or anything as long as it's available in the reference population of interest.",
+                metavar = "strata_name"),
+    make_option("--subgroups", type = "character", default = NULL,
+                help = "Subgroup variable name or list of variable names, separated by semi-colon (;) [default %default]. If subgroups are used, rate are standardised within those subgroups. Subgroups are analysed independently, not in combination. If NULL, rates will be calculated over the entire cohort only.",
+                metavar = "subgroup_varnames"),
+    make_option("--numerator", type="character", default = NULL,
+                help = "Name of the numerator variable in the cohort dataset [default %default]. This is usually an event count within a given period."),
+    make_option("--denominator", type="character", default = NULL,
+                help = "Name of the denominator variable in the cohort dataset [default %default]. This is usually a population count or time-at-risk."),
+    make_option("--reference_population", type="character", default = NULL,
+                help = "Filepath of the reference population used for standardisation. This dataset must be stratified by the strata defined in 'standardisation_strata'. NEEDS THOUGHT ABOUT HOW TO MAKE THESE AVAILABLE ON THE BACKEND OR NOT"),
+    make_option("--min_count", type = "integer", default = 6,
+                help = "The minimum permissable numerator and denominator counts for each rate in each subgroup [default %default].",
+                metavar = "min_count"),
+    make_option("--round_method", type = "character", default = "constant",
+                help = "Small number suppression method [default %default]. 'ceiling' rounds values up to the nearest multiple of 'min_count'. 'floor' rounds values down to the nearest multiple of 'min_count'. 'mid' rounds values to the nearest multiple of 'min_count', with a 'min_count/2' offset. 'redact' will set values less than 'min_count' to 'NA'",
+                metavar = "method"),
+    make_option("--make_plots", type = "logical", default = TRUE,
+                help = "Should plots of unstandardised and standardised rates for each subgroup be created in the output folder? [default %default]. These are fairly basic plots for sense-checking purposes only.",
+                metavar = "TRUE/FALSE")
+  )
+
+  opt_parser <- OptionParser(usage = "direct_standardisation:[version] [options]", option_list = option_list)
+  opt <- parse_args(opt_parser)
+
+  df_input <- opt$df_input
+  dir_output <- opt$dir_output
+  standardisation_strata <- opt$standardisation_strata
+  subgroups <- opt$subgroups
+  denominator <- opt$denominator
+  numerator <- opt$numerator
+  reference_population <- opt$reference_population
+  min_count <- opt$min_count
+  round_method <- opt$round_method
+  make_plots <- opt$make_plots
+}
+
+standardisation_strata <- str_split(standardisation_strata, ";")[[1]]
+subgroups <- str_split(subgroups, ";")[[1]]
+
+
+# it is assumed that the dataset contains a YYYY-MM-DD formatted "date" variable defining relevant calendar-time strata
+# maybe this should be defined explicitly as a "strata"
+
+## get measure id from filename, as specified in study definition
+measure_id <- str_remove(fs::path_ext_remove(fs::path_file(df_input)), "measure\\_")
+
+## define input/output directories ----
+fs::dir_create(fs::path(here(dir_output)))
+analysis_dir <- here("output", "analysis")
+
+
+## create rounding function for small number suppression ----
+
+osround <- function(x, min_count=5, method="mid"){
+  switch(
+    method,
+    "redact" = {if_else(x < min_count, NA_integer_, as.integer(x))},
+    "mid" = {ceiling(x/min_count)*min_count - (floor(min_count/2)*(x!=0))},
+    "floor" = {floor(x/min_count)*min_count},
+    "ceiling" = {ceiling(x/min_count)*min_count}
+  )
+}
+
+
+# import cohort rates data ----
+
+## import ----
+data_cohort0 <-read_csv(df_input) %>%
+  mutate(
+    # these operations should somehow be either automated or pre-applied because they ensure the reference population can be appropriately joined
+    year = as.integer(lubridate::year(date)),
+    sex = factor(sex, levels=c("F", "M"), labels= c("Female", "Male")),
+    ageband20year = cut(as.numeric(str_extract(ageband5year, "^\\d+")), c(0,20,40,60, 80, Inf), right = FALSE),
+    overall="overall"
+  )
+
+stopifnot("standardisation_strata not in cohort population" = all(standardisation_strata %in% names(data_cohort0)))
+
+## process ----
+data_cohort <-
+  data_cohort0  %>%
+  transmute(
+    !!! syms(subgroups),
+    !!! syms(standardisation_strata),
+    #death_1year = as.integer(death_1year),
+    numerator = as.integer(.data[[numerator]]),
+    denominator = as.integer(.data[[denominator]]),
+    rate = value,
+    date = as.Date(date),
+
+  ) %>%
+  arrange(
+    !!! syms(standardisation_strata),
+    date,
+  )
+
+# import reference population ----
+data_reference_everything <- arrow::read_feather(here("reference-populations", glue("{reference_population}.feather")))
+
+## test that standardisation_strata match between cohort and reference ----
+stopifnot("standardisation_strata not in reference population" = all(standardisation_strata %in% names(data_reference_everything)))
+
+## test that subgroup levels match between cohort and reference ----
+
+print("Do group levels in reference population match those in cohort population?")
+for(i in c(standardisation_strata)){
+  print(i)
+
+  referencelevels <- levels(factor(data_reference_everything[[i]]))
+  cohortlevels <- levels(factor(data_cohort[[i]]))
+  alllevels <- unique(c(referencelevels, cohortlevels))
+  inlevel <- cbind(
+      "in reference" = as.integer(alllevels %in% referencelevels),
+      "in cohort" = as.integer(alllevels %in% cohortlevels)
+    )
+  rownames(inlevel) <- alllevels
+  print(inlevel)
+}
+
+# apply weighting to get standardised rates ----
+
+## aggregate population counts by required standardisation_strata ----
+data_reference <-
+  data_reference_everything %>%
+  group_by(!!!syms(standardisation_strata)) %>%
+  summarise(
+    N_reference = sum(population),
+  ) %>%
+  ungroup()
+
+## join cohort rates and reference population size ----
+data_combined <-
+  left_join(
+    data_cohort,
+    data_reference,
+    by = c(standardisation_strata)
+  )
+
+## define function to standardise rates within specified subgroups ----
+rounded_rates <- function(data, min_count, method, ...){
+  data %>%
+    group_by(...) %>%
+    summarise(
+      numerator_sum = sum(numerator), # renamed to numerator_sum as unsummed numerator is needed later
+      denominator_sum = sum(denominator), # renamed to denominator_sum as unsummed denominator is needed later
+      N_reference_sum = sum(N_reference),
+
+      rate_unweighted = numerator_sum / denominator_sum,
+      var_rate_unweighted = rate_unweighted * (1 - rate_unweighted) * (1 / denominator_sum), # variance using aggregate counts
+      var_rate_unweighted1 = (1/(n()^2))* sum(rate * (1 - rate) * (1 / denominator)), # variance using sum of variance of unaggregated counts
+
+      rate_weighted = sum((numerator*N_reference) /  denominator) / N_reference_sum, # = sum(numerator * weight / denominator) = sum(weight*rate), where weight = (N_reference / sum(N_reference))
+      var_rate_weighted = sum(((N_reference/N_reference_sum)^2) * rate * (1 - rate) * (1 / denominator)), # = sum(weight^2 * rate * (1-rate) / denominator)
+      .groups="drop"
+    ) %>%
+    # rounding
+    mutate(
+      numerator_sum_rounded = osround(numerator_sum, min_count, method),
+      denominator_sum_rounded = osround(denominator_sum, min_count, method),
+      rate_unweighted_rounded = numerator_sum_rounded / denominator_sum_rounded,
+      var_rate_unweighted_rounded = (rate_unweighted_rounded * (1 - rate_unweighted_rounded)) * (1 / denominator_sum_rounded), # normal approximation
+    ) %>%
+    select(
+      ...,
+
+      rate_unweighted,
+      var_rate_unweighted,
+      var_rate_unweighted1,
+
+      #numerator_sum,
+      #denominator_sum,
+      rate_weighted,
+      var_rate_weighted,
+
+      numerator_sum_rounded,
+      denominator_sum_rounded,
+      rate_unweighted_rounded,
+      var_rate_unweighted_rounded,
+
+    )
+}
+
+## define function to plot stnadardised rates  within specified subgroups ----
+plot_standardised_rates <- function(data, group, ..., filename){
+
+
+  data_weighted <-
+    data %>%
+    transmute(
+      {{ group }},
+      ...,
+      weighted = "weighted",
+      rate = rate_weighted,
+      var = var_rate_weighted
+    )
+
+  data_unweighted <-
+    data %>%
+    transmute(
+      {{ group }},
+      ...,
+      weighted = "unweighted",
+      rate = rate_unweighted,
+      var = var_rate_unweighted
+    )
+
+  data_long <<-
+    bind_rows(data_weighted, data_unweighted) %>%
+    mutate(
+      rate.ll = rate + qnorm(0.025)*sqrt(var),
+      rate.ul = rate + qnorm(0.975)*sqrt(var)
+    )
+
+  plot_rates <-
+    data_long %>%
+    ggplot(aes(group={{ group }})) +
+    #geom_hline(aes(yintercept=0), colour="black")+
+    geom_line(aes(x=date, y=rate, colour={{ group }}))+
+    geom_ribbon(aes(x=date, ymin=rate.ll, ymax=rate.ul, fill={{ group }}), colour="transparent", alpha=0.05)+
+    facet_grid(rows=vars(weighted))+
+    scale_x_date(
+      labels = scales::label_date("%b"),
+      date_breaks = "2 months",
+      sec.axis = sec_axis(
+        trans = ~as.Date(.),
+        labels = scales::label_date("%Y")
+      )
+    )+
+    scale_color_brewer(type="qual", palette="Set1")+
+    scale_fill_brewer(type="qual", palette="Set1")+
+    labs(
+      x="Date",
+      y="Rate",
+      colour=NULL,
+      fill=NULL,
+      linetype=NULL
+    )+
+    theme_minimal()+
+    theme(
+      legend.position="bottom",
+      axis.ticks.x = element_line(),
+      axis.text.x = element_text(hjust=0),
+      strip.text.y = element_text(angle=0)
+    )
+
+  #print(plot_rates) # to test interactively
+
+
+  ggsave(
+    filename = filename,
+    plot = plot_rates
+  )
+
+}
+
+## test rates and variance are equal if no aggregation performed ----
+data_check <- rounded_rates(data_combined, min_count, round_method, 1, date, !!! syms(standardisation_strata))
+stopifnot("weighted rate should be the same as the unweighted rate if no agregation is used" = all(abs(data_check$rate_unweighted - data_check$rate_weighted)<(2^-15), na.rm = TRUE))
+
+## calculate rates overall (not within any subgroup) ----
+data_all <- rounded_rates(data_combined, min_count, round_method, date)
+
+
+## calculate subgroup specific rates (across all specified subgroups), write to file, and plot if requested ----
+
+for(i in subgroups){
+
+  sym_i <- sym(i)
+  rates_weighted <- rounded_rates(data_combined, min_count, round_method, {{ sym_i }}, date)
+  write_csv(rates_weighted, path = fs::path(here(dir_output), glue("rates_{i}.csv")))
+  if(make_plots){
+    plot_standardised_rates(
+      rates_weighted,
+      group = {{ sym_i }},
+      date,
+      filename=fs::path(here(dir_output), glue("rates_{i}.jpg"))
+    )
+  }
+}
+
--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -1,13 +1,201 @@
-from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv  # NOQA
 
-
-study = StudyDefinition(
-    default_expectations={
-        "date": {"earliest": "1900-01-01", "latest": "today"},
-        "rate": "uniform",
-        "incidence": 0.5,
-    },
-    population=patients.registered_with_one_practice_between(
-        "2019-02-01", "2020-02-01"
-    ),
+from cohortextractor import (
+  StudyDefinition,
+  patients,
+  codelist_from_csv,
+  codelist,
+  filter_codes_by_category,
+  combine_codelists,
+  Measure
 )
+
+start_date = "2018-01-01"
+
+# Specify study definition
+study = StudyDefinition(
+  
+  # Configure the expectations framework
+  default_expectations={
+    "date": {"earliest": "2018-01-01", "latest": "2020-01-01"},
+    "rate": "uniform",
+    "incidence": 0.1,
+    "int": {"distribution": "normal", "mean": 1000, "stddev": 100},
+    "float": {"distribution": "normal", "mean": 25, "stddev": 5},
+  },
+  
+  index_date = start_date,
+  
+  # This line defines the study population
+  population=patients.satisfying(
+    """
+      registered
+      AND
+      age >= 0 AND age <=110
+      AND
+      sex = "M" OR sex = "F" 
+      AND
+      NOT has_died
+    """,
+    
+    # we define baseline variables on the day _before_ the study date
+    registered=patients.registered_as_of(
+      "index_date",
+    ),
+    has_died=patients.died_from_any_cause(
+      on_or_before="index_date - 1 day",
+      returning="binary_flag",
+    ), 
+  ),
+  
+  
+  ###############################################################################
+  ## Demographics
+  ###############################################################################
+  
+  age=patients.age_as_of( 
+    "index_date",
+  ),
+  
+  
+  ageband5year=patients.categorised_as(
+    {
+      "": "DEFAULT",
+      "0-4"   : "age>=0 AND age<=4", 
+      "5-9"   : "age>=5 AND age<=9", 
+      "10-14" : "age>=10 AND age<=14", 
+      "15-19" : "age>=15 AND age<=19",
+      "20-24" : "age>=20 AND age<=24",
+      "25-29" : "age>=25 AND age<=29",
+      "30-34" : "age>=30 AND age<=34",
+      "35-39" : "age>=35 AND age<=39",
+      "40-44" : "age>=40 AND age<=44",
+      "45-49" : "age>=45 AND age<=49",
+      "50-54" : "age>=50 AND age<=54",
+      "55-59" : "age>=55 AND age<=59",
+      "60-64" : "age>=60 AND age<=64",
+      "65-69" : "age>=65 AND age<=69",
+      "70-74" : "age>=70 AND age<=74",
+      "75-79" : "age>=75 AND age<=79",
+      "80-84" : "age>=80 AND age<=84",
+      "85-89" : "age>=85 AND age<=89",
+      "90+" : "age>=90",
+    },
+    return_expectations={
+      "category":{"ratios": 
+        {
+        "0-4"   : 0.05,
+        "5-9"   : 0.05,
+        "10-14" : 0.05,
+        "15-19" : 0.05,
+        "20-24" : 0.05,
+        "25-29" : 0.05,
+        "30-34" : 0.05,
+        "35-39" : 0.05,
+        "40-44" : 0.05,
+        "45-49" : 0.05,
+        "50-54" : 0.1,
+        "55-59" : 0.05,
+        "60-64" : 0.05,
+        "65-69" : 0.05,
+        "70-74" : 0.05,
+        "75-79" : 0.05,
+        "80-84" : 0.05,
+        "85-89" : 0.05,
+        "90+"   : 0.05,
+        }
+      }
+    },
+  ),
+  
+  sex=patients.sex(
+    return_expectations={
+      "rate": "universal",
+      "category": {"ratios": {"M": 0.49, "F": 0.51}},
+      "incidence": 1,
+    }
+  ),
+
+  # msoa
+  msoa=patients.address_as_of(
+    "index_date",
+    returning="msoa",
+    return_expectations={
+      "rate": "universal",
+      "category": {"ratios": {"E02000001": 0.0625, "E02000002": 0.0625, "E02000003": 0.0625, "E02000004": 0.0625,
+        "E02000005": 0.0625, "E02000007": 0.0625, "E02000008": 0.0625, "E02000009": 0.0625, 
+        "E02000010": 0.0625, "E02000011": 0.0625, "E02000012": 0.0625, "E02000013": 0.0625, 
+        "E02000014": 0.0625, "E02000015": 0.0625, "E02000016": 0.0625, "E02000017": 0.0625}},
+    },
+  ),    
+  
+  # stp is an NHS administration region based on geography
+  stp=patients.registered_practice_as_of(
+    "index_date",
+    returning="stp_code",
+    return_expectations={
+      "rate": "universal",
+      "category": {
+        "ratios": {
+          "STP1": 0.1,
+          "STP2": 0.1,
+          "STP3": 0.1,
+          "STP4": 0.1,
+          "STP5": 0.1,
+          "STP6": 0.1,
+          "STP7": 0.1,
+          "STP8": 0.1,
+          "STP9": 0.1,
+          "STP10": 0.1,
+        }
+      },
+    },
+  ),
+  
+  # NHS administrative region
+  # FIXME can we get an equivalent using patient postcode not GP address?
+  region=patients.registered_practice_as_of(
+    "index_date",
+    returning="nuts1_region_name",
+    return_expectations={
+      "rate": "universal",
+      "category": {
+        "ratios": {
+          "North East": 0.1,
+          "North West": 0.1,
+          "Yorkshire and The Humber": 0.2,
+          "East Midlands": 0.1,
+          "West Midlands": 0.1,
+          "East": 0.1,
+          "London": 0.1,
+          "South East": 0.1,
+          "South West": 0.1
+          #"" : 0.01
+        },
+      },
+    },
+  ),
+  
+  death_date = patients.died_from_any_cause(
+      on_or_after="index_date",
+      returning="date_of_death",
+    ), 
+    
+  death_1year = patients.died_from_any_cause(
+      between=["index_date", "index_date + 364 days"], # doesn't do leap years, but `+ 1 year - 1 day` doesn't work
+      returning="binary_flag",
+    ), 
+  
+)
+
+measures = [
+
+    Measure(
+      id="death",
+      numerator="death_1year",
+      denominator="population",
+      group_by=["sex", "ageband5year", "region"],
+      small_number_suppression=False
+    ),
+
+]
+
